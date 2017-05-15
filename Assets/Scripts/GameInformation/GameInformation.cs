b1bfa3b06d422d231fbf7e40fb33e46db519ff4e
grin <dmitriy.grinevich.qa@gmail.com>
2017-05-15 22:58:26 +0300

    Make An RPG Episode 11: Saving and Loading(2 min +-)

---
+++ Assets/Scripts/GameInformation/GameInformation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CharacterClasses;
+using UnityEngine;
+
+public class GameInformation : MonoBehaviour
+{
+    public static string PlayerName { get; set; }
+    public static int PlayerLevel { get; set; }
+    public static BaseCharacterClass PlayerClass { get; set; }
+    public static int Stamina { get; set; }
+    public static int Endurance { get; set; }
+    public static int Intellect { get; set; }
+    public static int Strength { get; set; }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+}
---
+++ Assets/Scripts/SavingAndLoading/SaveInformation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavingAndLoading : MonoBehaviour
+{
+    public static void SaveAllInformation()
+    {
+        PlayerPrefs.SetInt("PlayerLevel", GameInformation.PlayerLevel);
+        PlayerPrefs.SetString("PlayerName", GameInformation.PlayerName);
+        PlayerPrefs.SetString("PlayerName", GameInformation.PlayerName);
+
+    }
+}
