7897a19afa6223c0f17db551117e31f70ceed5d6
davespoon <dmitriy.grinevich.qa@gmail.com>
2017-05-15 17:55:14 +0300

    Make An RPG Episode 10: Character Creation Part 2(video at 03:00)

--- Assets/Scripts/CreatePlayer/CreateNewCharacter.cs
+++ Assets/Scripts/CreatePlayer/CreateNewCharacter.cs
@@ -9,6 +9,7 @@ namespace CreatePlayer
 
         private bool _isMageClass;
         private bool _isWarrriorClass;
+        private string _playerName = "Player-Bla-Bla";
 
         // Use this for initialization
         void Start()
@@ -23,6 +24,7 @@ namespace CreatePlayer
 
         private void OnGUI()
         {
+            _playerName = GUILayout.TextArea(_playerName);
             _isMageClass = GUILayout.Toggle(_isMageClass, "Mage Class");
             _isWarrriorClass = GUILayout.Toggle(_isWarrriorClass, "Warrior Class");
 
@@ -36,12 +38,14 @@ namespace CreatePlayer
                 {
                     _newPlayer.PlayerClass = new BaseWarriorClass();
                 }
+                _newPlayer.PlayerName = _playerName;
                 _newPlayer.PlayerLevel = 1;
                 _newPlayer.Stamina = _newPlayer.PlayerClass.Stamina;
                 _newPlayer.Endurance = _newPlayer.PlayerClass.Endurance;
                 _newPlayer.Intellect = _newPlayer.PlayerClass.Intellect;
                 _newPlayer.Strenght = _newPlayer.PlayerClass.Strength;
 
+                Debug.Log("Player Name: " + _newPlayer.PlayerName);
                 Debug.Log("Player class: " + _newPlayer.PlayerClass.CharacterClassName);
                 Debug.Log("Player level: " + _newPlayer.PlayerLevel);
             }
