fff9d59229172eb54fe8b71c21e5d08026048518
grin <dmitriy.grinevich.qa@gmail.com>
2017-06-24 21:04:25 +0300

    Make An RPG Episode 23: Character Creation GUI

---
+++ Assets/Scripts/CreatePlayerGUI/CreatePlayerGUI.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePlayerGUI : MonoBehaviour
+{
+    public enum PlayerCreationStates
+    {
+        ClassSelection,
+        StatAlocation,
+        FinalSetUp
+    }
+
+    public static PlayerCreationStates currentState;
+    private DisplayCreatePlayerFunctions displayFunctions = new DisplayCreatePlayerFunctions();
+
+    void Start()
+    {
+        currentState = PlayerCreationStates.ClassSelection;
+    }
+
+    void Update()
+    {
+        switch (currentState)
+        {
+            case PlayerCreationStates.ClassSelection:
+                Debug.Log("Class selection");
+                break;
+            case PlayerCreationStates.StatAlocation:
+                Debug.Log("Class selection");
+                break;
+            case PlayerCreationStates.FinalSetUp:
+                Debug.Log("Class selection");
+                break;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (currentState == PlayerCreationStates.ClassSelection)
+        {
+            displayFunctions.DisplayClassSelections();
+            Debug.Log("Display player selection function");
+        }
+
+        if (currentState == PlayerCreationStates.StatAlocation)
+        {
+            displayFunctions.DisplayStatAlocation();
+            Debug.Log("Display player stat alocation function");
+        }
+
+        if (currentState == PlayerCreationStates.FinalSetUp)
+        {
+            displayFunctions.DisplayFinalSetUp();
+            Debug.Log("Display player final setup function");
+        }
+    }
+}
---
+++ Assets/Scripts/CreatePlayerGUI/DisplayCreatePlayerFunctions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayCreatePlayerFunctions : MonoBehaviour
+{
+    public void DisplayClassSelections()
+    {
+        
+    }
+
+    public void DisplayStatAlocation()
+    {
+        
+    }
+
+    public void DisplayFinalSetUp()
+    {
+        
+    }
+}
